f96350d72926c14dd12abc9771f4b2063ddb4146
eleonora <nebrudko18@gmail.com>
2021-08-03 13:39:15 +0300

    made fix

---
+++ dev/js/top-banner.js
@@ -0,0 +1,8 @@
+// -
+//   var toogleButton = document.getElementById("toogle-popup");
+// -
+//  var popup = document.getElementById("popup");
+
+// toogleButton.onclick = function () {
+//     popup.classList.toogle("top-banner__popup_active");
+// }
