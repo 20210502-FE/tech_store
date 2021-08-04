a48f7b801a282900ae26c73319ce017f590cb10a
vovadoronin <Doronin04101993@gmail.com>
2021-08-04 22:02:53 +0300

    Fix fotter.scss, footer.pug and main.js

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,42 +1,31 @@
-const firstButton = document.getElementsByClassName('footer__button-arrow')[0];
-const secondButton = document.getElementsByClassName('footer__button-arrow')[1];
-const thirdButton = document.getElementsByClassName('footer__button-arrow')[2];
-const fourthButton = document.getElementsByClassName('footer__button-arrow')[3];
-const fifthButton = document.getElementsByClassName('footer__button-arrow')[4];
+const accordions = document.querySelectorAll('.accordion');
 
-const firstList = document.getElementsByClassName('footer__menu-list')[0];
-const secondList = document.getElementsByClassName('footer__menu-list')[1];
-const thirdList = document.getElementsByClassName('footer__menu-list')[2];
-const fourthList = document.getElementsByClassName('footer__menu-list')[3];
-const fifthList = document.getElementsByClassName('footer__menu-list')[4];
+const openAccordion = accordion => {
+    const accordionContent = accordion.querySelector('.footer__menu-list');
+    accordionContent.classList.add('footer__menu-list_active');
+    const arrow = accordion.querySelector('.footer-arrow');
+    arrow.classList.add('footer-arrow_top');
+};
 
-const firstImage = document.getElementsByClassName('footer-arow')[0];
-const secondImage = document.getElementsByClassName('footer-arow')[1];
-const thirdImage = document.getElementsByClassName('footer-arow')[2];
-const fourthImage = document.getElementsByClassName('footer-arow')[3];
-const fifthImage = document.getElementsByClassName('footer-arow')[4];
+const closeAccordion = accordion => {
+    const accordionContent = accordion.querySelector('.footer__menu-list');
+    accordionContent.classList.remove('footer__menu-list_active');
+    const arrow = accordion.querySelector('.footer-arrow');
+    arrow.classList.remove('footer-arrow_top');
+};
 
-firstButton.onclick = function () {
-    firstList.classList.toggle("footer__menu-list_active");
-    firstImage.classList.toggle("footer-arrow_top");
-}
+accordions.forEach((accordion) => {
+    const accordionHeading = accordion.querySelector('.accordion__heading');
+    const accordionContent = accordion.querySelector('.footer__menu-list');
 
-secondButton.onclick = function () {
-    secondList.classList.toggle("footer__menu-list_active");
-    secondImage.classList.toggle("footer-arrow_top");
-}
-
-thirdButton.onclick = function () {
-    thirdList.classList.toggle("footer__menu-list_active");
-    thirdImage.classList.toggle("footer-arrow_top");
-}
-
-fourthButton.onclick = function () {
-    fourthList.classList.toggle("footer__menu-list_active");
-    fourthImage.classList.toggle("footer-arrow_top");
-}
-
-fifthButton.onclick = function () {
-    fifthList.classList.toggle("footer__menu-list_active");
-    fifthImage.classList.toggle("footer-arrow_top");
-}
+    accordionHeading.onclick = () => {
+        if (accordionContent.classList.contains('footer__menu-list_active')) {
+            closeAccordion(accordion);
+        } else {
+            accordions.forEach((accordion) => {
+                closeAccordion(accordion);
+            });
+            openAccordion(accordion);
+        };
+    };
+});
